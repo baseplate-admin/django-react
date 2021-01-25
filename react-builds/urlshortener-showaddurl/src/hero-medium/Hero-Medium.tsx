ae8accb1c32114fd8c5ff2c9d9f2ef02135b3ab3
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2021-01-25 13:16:36 +0600

    I am removing django as its too much of a pain to deal with

---
+++ react-builds/urlshortener-showaddurl/src/hero-medium/Hero-Medium.tsx
@@ -0,0 +1,13 @@
+export default function HeroMedium(){
+    return(
+        <section className="hero is-medium">
+        <div className="hero-body" style={{textAlign: "center"}}>
+            <div className="container">
+                <h1 className="title">Thanks for using our service.</h1>
+                <h2 className="subtitle">See you soon!!</h2>
+            </div>
+        </div>
+    </section>
+    )
+
+}
---
+++ react-builds/urlshortener-showaddurl/src/short-url-show/ShortUrl.tsx
@@ -0,0 +1,15 @@
+export default function ShortUrl(){
+    return(
+        <section className="hero is-medium">
+        <div className="hero-body" style={{textAlign: "center"}}>
+            <div className="container">
+                <h1 className="title">Here is your shortened URL:</h1>
+                <h2 className="subtitle">
+                    <div id="Add">
+                    </div>
+                </h2>
+            </div>
+        </div>
+    </section>
+    )
+}
